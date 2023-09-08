b3d9b460958d15283a651a00ce5b98b87333dd27
Daniel Stiven Martinez <danimarcast@icloud.com>
2023-09-07 19:58:45 -0600

    Add

--- Regresion/RegresionLinealSimple.py
+++ Regresion/RegresionLinealSimple.py
@@ -2,3 +2,12 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+datos =  pd.read_csv("/Users/danimathud/Documents/GitHub/PythonMachineLearningProjects/Regresion/Salary_Data.csv")
+x = datos.iloc[:,0].values
+y = datos.iloc[:,1].values
+
+
+#Division de datos de prueba y entrenamiento
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state=1234)
