ad5bce3a4a5262178b9856569cef7a8b024af8eb
Daniel Stiven Martinez <danimarcast@icloud.com>
2023-09-26 14:59:46 -0600

    Add

---
+++ Clasificacion/NaiveBayes.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+from sklearn.neighbors import KNeighborsClassifier
+from matplotlib.colors import ListedColormap
+from sklearn.svm import SVC
+
