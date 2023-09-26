255df3fea5bb40280dd94caa025d5d2130b19d84
Daniel Stiven Martinez <danimarcast@icloud.com>
2023-09-26 15:05:43 -0600

    Add

--- Clasificacion/KernelSVM.py
+++ Clasificacion/KernelSVM.py
@@ -26,8 +26,9 @@ x_train = escalador.fit_transform(x_train)
 x_test = escalador.transform(x_test)
 
 # Implementacion del algoritmo.........................
+from sklearn.naive_bayes import GaussianNB
 
-clasificador = SVC(kernel = "poly", degree = 5, random_state = 1234)
+clasificador = GaussianNB()
 clasificador.fit(x_train,y_train)
 
 y_pred = clasificador.predict(x_test)
@@ -42,12 +43,12 @@ X_set, y_set = escalador.inverse_transform(x_train), y_train
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.25),
                      np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 100))
 plt.contourf(X1, X2, clasificador.predict(escalador.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('orange', 'blue')))
+             alpha = 0.75, cmap = ListedColormap(('#008B8B', 'darkviolet')))
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'darkblue'))(i), label = j)
-plt.title('SVM con Kernel Polinomial de grado 5 (Datos Entrenamiento)')
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('Green', 'fuchsia'))(i), label = j)
+plt.title('Naive Bayes (Datos Entrenamiento)')
 plt.xlabel('Edad')
 plt.ylabel('Salario')
 plt.legend()
