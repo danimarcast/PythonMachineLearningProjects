cb136f867f4c1c26a71c27934e8679f37df02246
Daniel Stiven Martinez <danimarcast@icloud.com>
2023-09-29 19:49:16 -0600

    Add

---
+++ AprendizajeReforzado/LimiteConfianzaSuperior.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+datos = pd.read_csv("./Ads_CTR_Optimisation.csv")
+
--- ReglasDeAsociacion/Apriori.py
+++ ReglasDeAsociacion/Apriori.py
@@ -2,11 +2,14 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-datos = pd.read_csv("/Users/danimathud/Documents/GitHub/PythonMachineLearningProjects/ReglasDeAsociacion/Market_Basket_Optimisation.csv",header = None)
+datos = pd.read_csv("./Market_Basket_Optimisation.csv",header = None)
 
 transaccion = []
-for i in range(0, 7501):
-  transaccion.append([str(datos.values[i,j]) for j in range(0, 20)])
+for i in range(0,7501):
+  transaccion.append([])
+  for j in range(0,20):
+    transaccion[i].append(str(datos.values[i,j]))
+
 # Entrenando el apriori
 from apyori import apriori
 reglas = apriori(transaccion, min_support = 0.003,min_confidence = 0.2, min_lift = 3,min_length= 2)
@@ -14,3 +17,7 @@ reglas = apriori(transaccion, min_support = 0.003,min_confidence = 0.2, min_lift
 #visualizacion
 resultados = list(reglas)
 print(resultados[3])
+
+transaccion = [transaccion.append([str(datos.values[i,j]) for j in range(0, 20)]) for i in range(0, 7501)]
+
+
