b8887313223cc5f0b911731fc93280124d5b07c3
Daniel Stiven Martinez <danimarcast@icloud.com>
2023-10-04 23:42:43 -0600

    Add

--- DataPreProcessing/filtrado.py
+++ DataPreProcessing/filtrado.py
@@ -8,23 +8,41 @@ curp =  []
 años_deseados =[93,94,95,96,97,98,99,0,1,2,3,4,5,6,7,8,9,10,11]
 
 años = []
-filtrados = []
+filtrados= []
 
-print(usuarios.shape)
 for i in range(0,2017):
-    if type(usuarios["CURP"][i]) == int:
-        usuarios.drop(i, axis = 0)
-usuarios = usu
-
-print(usuarios.shape)
+    if type(usuarios["CURP"][i]) == int or len(usuarios["CURP"][i]) != 18:
+        usuarios = usuarios.drop(i, axis = 0)
+        
 usuarios.reset_index(drop = True, inplace = True)
-print(usuarios.shape)
 
-# for i in range(0,len(usuarios)):
-#     for j in range(0,10):
-#         if usuarios["CURP"][i][4] != str(j):
-#             usuarios.drop(i, axis = 0)
-            
-            
+for j in range(0,len(usuarios)):
+    for i in range(0,10):
+        if usuarios["CURP"][j][4] != str(i):
+           usuarios.drop(j, axis = 0)
+           usuarios.reset_index(drop = True, inplace = True)
 
-# print(usuarios.shape)˙
+usuarios = usuarios.drop(532, axis = 0)
+usuarios.reset_index(drop = True, inplace = True)
+
+usuarios = usuarios.drop(594, axis = 0)
+usuarios.reset_index(drop = True, inplace = True)
+
+usuarios = usuarios.drop(751, axis = 0)
+usuarios.reset_index(drop = True, inplace = True)
+
+usuarios = usuarios.drop(1062, axis = 0)
+usuarios.reset_index(drop = True, inplace = True)
+
+for i in range(0,len(usuarios)):
+    for j in range(0,len(años_deseados)):
+        if int(usuarios["CURP"][i][4:6]) == años_deseados[j]:
+            if usuarios["CURP"][i][11:13]== "GT":
+                filtrados.append(usuarios["CURP"][i])
+                
+
+file = open('filtrados.txt','w')
+for  usuario in filtrados:
+	file.write(usuario +"\n")
+file.close()
+        
