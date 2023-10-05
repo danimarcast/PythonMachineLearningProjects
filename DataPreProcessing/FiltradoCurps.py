dfced119bcc60b6143f90ebfc9769e8f83fa9ae0
Daniel Stiven Martinez <danimarcast@icloud.com>
2023-10-05 01:39:29 -0600

    Add

--- DataPreProcessing/FiltradoCurps.py
+++ DataPreProcessing/FiltradoCurps.py
@@ -9,6 +9,7 @@ años_deseados =[93,94,95,96,97,98,99,0,1,2,3,4,5,6,7,8,9,10,11]
 
 años = []
 filtrados= []
+indices = []
 
 for i in range(0,2017):
     if type(usuarios["CURP"][i]) == int or len(usuarios["CURP"][i]) != 18:
@@ -16,23 +17,21 @@ for i in range(0,2017):
         
 usuarios.reset_index(drop = True, inplace = True)
 
+#cont = 0
 for j in range(0,len(usuarios)):
+    cont = 0
     for i in range(0,10):
-        if usuarios["CURP"][j][4] != str(i):
-           usuarios.drop(j, axis = 0)
-           usuarios.reset_index(drop = True, inplace = True)
+        if usuarios["CURP"][j][4] == str(i):
+            break
+        else:
+            cont = cont + 1
+            i = i+1
+            if cont == 10:
+                print("ok")
+                usuarios = usuarios.drop(j, axis = 0)
+                
+usuarios.reset_index(drop = True, inplace = True)           
 
-usuarios = usuarios.drop(532, axis = 0)
-usuarios.reset_index(drop = True, inplace = True)
-
-usuarios = usuarios.drop(594, axis = 0)
-usuarios.reset_index(drop = True, inplace = True)
-
-usuarios = usuarios.drop(751, axis = 0)
-usuarios.reset_index(drop = True, inplace = True)
-
-usuarios = usuarios.drop(1062, axis = 0)
-usuarios.reset_index(drop = True, inplace = True)
 
 for i in range(0,len(usuarios)):
     for j in range(0,len(años_deseados)):
@@ -43,6 +42,5 @@ for i in range(0,len(usuarios)):
 
 file = open('filtrados.txt','w')
 for  usuario in filtrados:
-	file.write(usuario +"\n")
+ 	file.write(usuario +"\n")
 file.close()
-        
